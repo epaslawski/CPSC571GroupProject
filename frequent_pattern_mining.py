f12d4d48afde150b85c7361b6a1a214c00f85d6d
Alyssa <bohyeon.cha@ucalgary.ca>
2021-11-16 18:44:20 -0700

    Templates for fpm() and confidence()

---
+++ frequent_pattern_mining.py
@@ -0,0 +1,11 @@
+def fpm(population: list[Person]) -> list: 
+    # do fpm here
+
+    return frequent_sets
+
+
+def confidence(infected_person: Person, susceptible_person: Person) -> int:
+    # calculate confidence here
+
+    return confidence
+
