1d3108e51247fc631c84d96e71685dbafbabb21a
Alyssa <bohyeon.cha@ucalgary.ca>
2021-11-25 19:48:05 -0700

    Description of parameters

---
+++ parameters.py
@@ -0,0 +1,5 @@
+"""
+BETA: the expected amount of people an infected person infects per day
+D: number of days an infected person has and can spread the disease
+GAMMA: the proportion of infected recovering per day (γ = 1/D)
+"""
