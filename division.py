705a68e25b329e728cfc6825fa334ef07ce72823
Alyssa <bohyeon.cha@ucalgary.ca>
2021-12-14 22:27:14 -0700

    Create function to calculate total_population

--- division.py
+++ division.py
@@ -9,7 +9,7 @@ class Division:
         self.name = name
         self.populaton = population
         self.age_0_4 = age_0_4
-        self.age_5_9 = age_5_9]
+        self.age_5_9 = age_5_9
         self.age_10_14 = age_10_14
         self.age_15_19 = age_15_19
         self.age_20_64 = age_20_64
@@ -35,7 +35,7 @@ def initialize_divisons():
     All divisions from the data are parsed and initialized inside this method.
     '''
 
-    divisions = []
+    divisions = [] 
 
     with open('data.csv', mode='r') as data_file:
 
@@ -43,7 +43,7 @@ def initialize_divisons():
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
-                print(f'{", ".join(row)}')
+                #print(f'{", ".join(row)}')
                 line_count += 1
             else:
                 name = row["Division"]
@@ -58,9 +58,28 @@ def initialize_divisons():
                 pop_density = row["Population density"]
                 emp_rate = row["Employment Rate"]
                 worked = row["Worked at usual place"]
-                divisions.append(Division(name, population, age_0_4, age_5_9, age_10_14,\
-                                            age_15_19, age_20_64, age_65, avg_household_size,\
-                                            pop_density, emp_rate, worked ))               
+                divisions.append(Division(name, int(population), int(age_0_4), int(age_5_9), int(age_10_14),\
+                                            int(age_15_19), int(age_20_64), int(age_65), int(avg_household_size),\
+                                            int(pop_density), int(emp_rate), int(worked) ))
     return divisions
-            
-        
+
+
+def get_total_pop():
+
+    total_workplaces = 0
+
+    with open('data.csv', mode='r') as data_file:
+
+        csv_reader = csv.DictReader(data_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                worked = row["Worked at usual place"]
+                total_workplaces += int(worked)
+    
+    total_workplaces = total_workplaces/80         #assume 80 people work at one place on average
+    return total_workplaces
+
+
