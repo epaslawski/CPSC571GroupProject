197910a492055e5a5cc3c949fb9e542e32d1cecc
Alyssa <bohyeon.cha@ucalgary.ca>
2021-12-15 01:43:21 -0700

    Modify person attributes

--- person.py
+++ person.py
@@ -1,16 +1,14 @@
-from utilities import contagious_rating, generate_household, generate_workplace
 
 class Person:
 
-    def __init__(self, age, division):
+    def __init__(self, age, works, household, workplace, division):
         self.age = age
+        self.works = works
+        self.household = household
+        self.workplace = workplace
         self.division = division
-        self.contagious_rating = contagious_rating(age)
-        self.household = generate_household(division)
-        self.workplace = generate_workplace(division)
-        self.days_infectious = 10               # number of days this person is infectious
         
-    
+   
     
     
 
