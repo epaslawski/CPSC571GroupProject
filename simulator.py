1bbffcff857658a9e22f394621954defaa581417
epaslawski <erin.paslawski@gmail.com>
2021-12-21 11:42:19 -0700

    Adding initial disease progression and removing older simulator

--- simulator.py
+++ simulator.py
@@ -2,55 +2,59 @@ from scipy.stats import norm
 import random
 import time
 import pandas as pd
-from .person import Person
-from .classifier import *
-from .division import *
-from .utilities import *
+from person import Person
+from classifier import *
+from division import *
+from utilities import *
 import csv
 
 
-def run_day(div, day) -> list:
-    
+
+def run_day(div, day):
     name = div.name
     res_total = len(div.residents)
-    random.shuffle(div.residents)
-    num_test = (res_total * 17) // 100 # 1/6 of population
-    # create test list
-    test_list = []
-    # create training list
-    training_list = []
 
-    for x in range (0,res_total):
-        per = div.residents[x]
+    if (day > 0 ):
 
-        if len(test_list) < num_test:
-            if (per.infected == False):
-                test_list.append(per)
+        random.shuffle(div.residents)
+        num_test = (res_total * 17) // 100 # 1/6 of population
+        # create test list
+        test_list = []
+        # create training list
+        training_list = []
+
+        for x in range (0,res_total):
+            per = div.residents[x]
+
+            if len(test_list) < num_test:
+                if (per.infected == False):
+                    test_list.append(per)
+                else:
+                    training_list.append(per)
             else:
                 training_list.append(per)
-        else:
-            training_list.append(per)
 
 
-    # run classifier
-    classifier_out = classifier(test_list, training_list)
+        # run classifier
+        classifier_out = classifier(test_list, training_list)
     #return data list to add to csv
     active = 0
 
     for per in div.residents:
         if per.infected == True:
             active += 1
-    return [name, x, active,res_total]
+    return [name, day, active,res_total]
 
 def main():
-    # create divisions and populations
-    ds = initialize_divisions()
 
     # create output csv
     headers = ['division', 'day', 'active_cases', 'population']
 
     sim_days = int(input("How many days to run the simulator: "))
+    init_infect_pct = int(input("What percentage of the population is infected at day 0: "))
 
+    # create divisions and populations
+    ds = initialize_divisions(init_infect_pct)
     with open('sim_out.csv', 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
 
@@ -60,9 +64,8 @@ def main():
         for x in range(0,sim_days):
             for div in ds:
                 data = run_day(div, x)
+                print(data)
                 writer.writerow(data)
-
-    
 
 if __name__ == "__main__":
     main()
